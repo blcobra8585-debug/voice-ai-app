1eeca5030fe089432b175d2240c711b0161ad5d8
blcobra8585-debug <blcobra8585@gmail.com>
2026-05-09 02:52:08 +0530

    fix(voice-changer): add missing expo-env.d.ts

---
+++ artifacts/voice-changer/expo-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="expo/types" />
+
+// NOTE: This file should not be edited and should be in your git ignore file.
+// It is used to ensure that the @expo/metro-runtime types are used when running in the browser.
